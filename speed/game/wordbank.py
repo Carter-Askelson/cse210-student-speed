3bfdbc59d302749c45f439689b14cdf3cd01b48c
Emma Burkett <bur20089@byui.edu>
2021-10-06 00:47:27 -0600

    get_words method done - success in reaching the txt file

--- speed/game/wordbank.py
+++ speed/game/wordbank.py
@@ -1,4 +1,4 @@
-
+import random
 
 class WordBank():
     """handles generating words
@@ -14,11 +14,20 @@ class WordBank():
     def __init__(self):
             
         self.words_list = []
+        #print(self.get_words()) #uncomment to view results of get_words
 
-    def get_words(self, number):
+    def get_words(self):
         #get the next number of words from word.txt
-        pass
+        i = 0
+        while i < 5:
+            lines = open('speed\game\words.txt').read().splitlines()
+            self.words_list.append(random.choice(lines))
+            i += 1
+        return self.words_list
+
 
     def return_words(self):
         self.get_words()
         return self.word_list
+
+#WordBank().get_words() #uncomment to view results of get_words
