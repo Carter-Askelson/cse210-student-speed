0e40db199b465a0bfbdb2a127fede02fc891859e
Emma Burkett <bur20089@byui.edu>
2021-10-06 22:12:25 -0600

    working on buffer

--- speed/game/buffer.py
+++ speed/game/buffer.py
@@ -11,8 +11,10 @@ class Buffer(arcade.Window):
         
     """
 
-    def __init__(self):
-        self.new_word = ""
+    def __init__(self):#self, width, height, words, word_rows_count=20):
+        #super().__init__(width, height, title="Space Typer")
+        self.user_input = ""
+        self.keep_playing = True
 
     def on_key_press(self, key):
         
@@ -24,65 +26,64 @@ class Buffer(arcade.Window):
        Returns:
             string: The letter that was typed.
        """
-        user_input = ""
-        if key == arcade.key.Q:
-            user_input += "q"
-        elif key == arcade.key.W:
-            user_input += "w"
-        elif key == arcade.key.E:
-            user_input += "e"
-        elif key == arcade.key.R:
-            user_input += "r"
-        elif key == arcade.key.T:
-            user_input += "t"
-        elif key == arcade.key.Y:
-            user_input += "y"
-        elif key == arcade.key.U:
-            user_input += "u"
-        elif key == arcade.key.I:
-            user_input += "i"
-        elif key == arcade.key.O:
-            user_input += "o"
-        elif key == arcade.key.P:
-            user_input += "p"
-        elif key == arcade.key.A:
-            user_input += "a"
-        elif key == arcade.key.S:
-            user_input += "s"
-        elif key == arcade.key.D:
-            user_input += "d"
-        elif key == arcade.key.F:
-            user_input += "f"
-        elif key == arcade.key.G:
-            user_input += "g"
-        elif key == arcade.key.H:
-            user_input += "h"
-        elif key == arcade.key.J:
-            user_input += "j"
-        elif key == arcade.key.K:
-            user_input += "k"
-        elif key == arcade.key.L:
-            user_input += "l"
-        elif key == arcade.key.Z:
-            user_input += "z"
-        elif key == arcade.key.X:
-            user_input += "x"
-        elif key == arcade.key.C:
-            user_input += "c"
-        elif key == arcade.key.V:
-            user_input += "v"
-        elif key == arcade.key.B:
-            user_input += "b"
-        elif key == arcade.key.N:
-            user_input += "n"
-        elif key == arcade.key.M:
-            user_input += "m"
-        elif key == arcade.key.ESCAPE:
-            return False
-        elif key == arcade.key.ENTER:
-            return user_input
-        else:
-            user_input += ""
+        while key != arcade.key.ENTER and self.keep_playing == True:
+            if key == arcade.key.Q:
+                self.user_input += "q"
+            elif key == arcade.key.W:
+                self.user_input += "w"
+            elif key == arcade.key.E:
+                self.user_input += "e"
+            elif key == arcade.key.R:
+                self.user_input += "r"
+            elif key == arcade.key.T:
+                self.user_input += "t"
+            elif key == arcade.key.Y:
+                self.user_input += "y"
+            elif key == arcade.key.U:
+                self.user_input += "u"
+            elif key == arcade.key.I:
+                self.user_input += "i"
+            elif key == arcade.key.O:
+                self.user_input += "o"
+            elif key == arcade.key.P:
+                self.user_input += "p"
+            elif key == arcade.key.A:
+                self.user_input += "a"
+            elif key == arcade.key.S:
+                self.user_input += "s"
+            elif key == arcade.key.D:
+                self.user_input += "d"
+            elif key == arcade.key.F:
+                self.user_input += "f"
+            elif key == arcade.key.G:
+                self.user_input += "g"
+            elif key == arcade.key.H:
+                self.user_input += "h"
+            elif key == arcade.key.J:
+                self.user_input += "j"
+            elif key == arcade.key.K:
+                self.user_input += "k"
+            elif key == arcade.key.L:
+                self.user_input += "l"
+            elif key == arcade.key.Z:
+                self.user_input += "z"
+            elif key == arcade.key.X:
+                self.user_input += "x"
+            elif key == arcade.key.C:
+                self.user_input += "c"
+            elif key == arcade.key.V:
+                self.user_input += "v"
+            elif key == arcade.key.B:
+                self.user_input += "b"
+            elif key == arcade.key.N:
+                self.user_input += "n"
+            elif key == arcade.key.M:
+                self.user_input += "m"
+            elif key == arcade.key.ESCAPE:
+                self.keep_playing = False
+                break
+            else:
+                self.user_input += ""
         
             
 
